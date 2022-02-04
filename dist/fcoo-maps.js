2988ff4bc1b99ca1267e78796068a6ae2053d86a
Niels Holt <niels@steenbuchholt.dk>
2022-02-04 12:43:06 +0100

    FCOO-logo in browser console :-)

--- dist/fcoo-maps.js
+++ dist/fcoo-maps.js
@@ -313,8 +313,6 @@ control-route.js
 (function (/*window, document, undefined*/) {
     "use strict";
 
-    /* eslint-disable no-console */
-
     /*
 
     ████████  ██████    ██████    ██████
@@ -338,24 +336,100 @@ control-route.js
     ██        ██    ██   ██    ██   ██    ██
     ██         ██████     ██████     ██████
 
-    */
+
+XXXXXX   XXXX     XXXX     XXXX
+X       X    X   X    X   X    X
+X       X        X    X   X    X
+XXXX    X        X    X   X    X
+X       X        X    X   X    X
+X       X    X   X    X   X    X
+X        XXXX     XXXX     XXXX
 
 
+ XXXX    XXXXXX   XXXX   X     X  XXXXXX  XXXXXXX   XXXX    XXXX
+X    X   X       X    X  XX   XX  X          X     X    X  X    X
+X        X       X    X  X X X X  X          X     X    X  X
+X        XXXX    X    X  X  X  X  XXXX       X     X    X  X
+X   XXX  X       X    X  X     X  X          X     X    X  X
+X    XX  X       X    X  X     X  X          X     X    X  X    X
+ XXXX X  XXXXXX   XXXX   X     X  XXXXXX     X      XXXX    XXXX
+
+
+XXXXXXXXXXXXXXXX  XXXXXXXX        XXXXXXXX         XXXXXXXX
+XXXXXXXXXXXXXXX XXXXXXXXXXXX    XXXXXXXXXXXX     XXXXXXXXXXXX
+XXXX           XXXX      XXXX  XXXX      XXXX   XXXX      XXXX
+XXXX          XXXX       XXXX XXXX        XXXX XXXX        XXXX
+XXXX          XXXX            XXXX        XXXX XXXX        XXXX
+XXXX          XXXX            XXXX        XXXX XXXX        XXXX
+XXXXXXXXXX    XXXX            XXXX        XXXX XXXX        XXXX
+XXXXXXXXXX    XXXX            XXXX        XXXX XXXX        XXXX
+XXXX          XXXX            XXXX        XXXX XXXX        XXXX
+XXXX          XXXX            XXXX        XXXX XXXX        XXXX
+XXXX          XXXX       XXXX XXXX        XXXX XXXX        XXXX
+XXXX          XXXX       XXXX  XXXX      XXXX   XXXX      XXXX
+XXXX            XXXXXXXXXXXX    XXXXXXXXXXXX     XXXXXXXXXXXX
+XXXX              XXXXXXXX        XXXXXXXX         XXXXXXXX
+          Forsvarets Center for Operativ Oceanografi
+                    fcoo.dk - info@fcoo.dk
+*/
+
+/*
     var text = [
-        ' ████████   ██████     ██████     ██████',
-        ' ██        ██    ██   ██    ██   ██    ██',
-        ' ██        ██         ██    ██   ██    ██',
-        ' █████     ██         ██    ██   ██    ██',
-        ' ██        ██         ██    ██   ██    ██',
-        ' ██        ██    ██   ██    ██   ██    ██',
-        ' ██         ██████     ██████     ██████',
-        'Forsvarets Center for Operativ Oceanografi',
-        '         fcoo.dk  - info@fcoo.dk'
+        '████████████████  ████████        ████████         ████████',
+        '███████████████ ████████████    ████████████     ████████████',
+        '████           ████      ████  ████      ████   ████      ████ ',
+        '████          ████       ████ ████        ████ ████        ████',
+        '████          ████            ████        ████ ████        ████',
+        '████          ████            ████        ████ ████        ████',
+        '██████████    ████            ████        ████ ████        ████',
+        '██████████    ████            ████        ████ ████        ████',
+        '████          ████            ████        ████ ████        ████',
+        '████          ████            ████        ████ ████        ████',
+        '████          ████       ████ ████        ████ ████        ████',
+        '████          ████       ████  ████      ████   ████      ████ ',
+        '████            ████████████    ████████████     ████████████',
+        '████              ████████        ████████         ████████',
+        '          Forsvarets Center for Operativ Oceanografi',
+        '                    fcoo.dk - info@fcoo.dk',
     ];
+    var text = [
+        'XXXXXXXXXXXXX  XXXXXXXX        XXXXXXXX         XXXXXXXX',
+        'XXXXXXXXXXXX XXXXXXXXXXXX    XXXXXXXXXXXX     XXXXXXXXXXXX',
+        'XXXX        XXXX      XXXX  XXXX      XXXX   XXXX      XXXX ',
+        'XXXX       XXXX       XXXX XXXX        XXXX XXXX        XXXX',
+        'XXXXXXXXX  XXXX            XXXX        XXXX XXXX        XXXX',
+        'XXXXXXXXX  XXXX            XXXX        XXXX XXXX        XXXX',
+        'XXXX       XXXX       XXXX XXXX        XXXX XXXX        XXXX',
+        'XXXX       XXXX       XXXX  XXXX      XXXX   XXXX      XXXX ',
+        'XXXX         XXXXXXXXXXXX    XXXXXXXXXXXX     XXXXXXXXXXXX',
+        'XXXX            XXXXXXXX        XXXXXXXX         XXXXXXXX',
+        '         Forsvarets Center for Operativ Oceanografi          ',
+        '                    fcoo.dk - info@fcoo.dk',
+    ];
+
+*/
+    var text = [
+        '   █████████',
+        ' █████████  ',
+        '████        ████████████        ████████████         ████████████',
+        '████      ████████████████    ████████████████     ████████████████',
+        '████     ████          ████  ████          ████   ████          ████ ',
+        '███████ ████           ████ ████            ████ ████            ████',
+        '███████ ████                ████            ████ ████            ████',
+        '████    ████                ████            ████ ████            ████',
+        '████    ████           ████ ████            ████ ████            ████',
+        '████    ████           ████  ████          ████   ████          ████ ',
+        '████      ████████████████    ████████████████     ████████████████',
+        '████        ████████████        ████████████         ████████████',
+        '             Forsvarets Center for Operativ Oceanografi',
+        '                       fcoo.dk - info@fcoo.dk',
+    ];
+
+    /* eslint-disable no-console */
     for (var i=0; i<text.length; i++)
         console.log(text[i]);
-
     /* eslint-enable no-console */
+
 }(this, document));
 
 
