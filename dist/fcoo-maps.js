aad7bab5b52b2c172b8a000ffd08d3f849a36f54
Niels Holt <niels@steenbuchholt.dk>
2024-06-20 14:11:20 +0200

    New feature/method: nsMap._addMapContextmenu = function(header, method, index=-1): Add extra common map context-menu items from specific layers

--- dist/fcoo-maps.js
+++ dist/fcoo-maps.js
@@ -2819,6 +2819,17 @@ Global context-menu for all maps
         nsMap = ns.map = ns.map || {};
 
 
+    /***********************************************
+    nsMap._addMapContextmenu = function(header, method, index=-1)
+    Add extra common map context-menu items from specific layers
+    ***********************************************/
+    var mapContextmenu = nsMap.mapContextmenu = [];
+    nsMap._addMapContextmenu = nsMap._addMapContextMenu = function(header, method, index=-1){
+        index = index == -1 ? mapContextmenu.length : index;
+        mapContextmenu.push({header: header, method: method, index: index});
+        mapContextmenu.sort((obj1, obj2) => obj1.index - obj2.index);
+    };
+
     var map_contextmenu_header = {
             icon: 'fa-map',
             text: {da:'Kort', en:'Map'}
@@ -2862,10 +2873,23 @@ Global context-menu for all maps
 
 
     L.Map.addInitHook(function () {
+        //Convert context-menu-items from nsMap.mapContextmenu
+        let list = [];
+        mapContextmenu.forEach( (item,index) => {
+            list.push({
+                icon: item.header.icon,
+                text: item.header.text,
+                spaceBefore: index == 0,
+                spaceAfter: index == mapContextmenu.length-1,
+                onClick: function(id, latlng, $button, map){ item.method(latlng, map); }
+            });
+        });
+
         //If the map has a BsPositionControl with contextmenu or a options.MANGLER = true
         if ( (this.options.bsPositionOptions && this.options.bsPositionOptions.inclContextmenu) || (this.options.MANGLER) ){
-            this.setContextmenuHeader( map_contextmenu_header  );
-            this.addContextmenuItems( map_contextmenu_itemList);
+            this.setContextmenuHeader( map_contextmenu_header );
+            this.addContextmenuItems( list );
+            this.addContextmenuItems( map_contextmenu_itemList );
         }
     });
 }(jQuery, L, this, document));
